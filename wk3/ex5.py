12b97a1f48c78c8a3b88244bb59db90da1063c0a
Ed Weadon <ed@weadon.net>
2019-05-24 16:08:58 -0400

    updated ex5.py
    Signed-off-by: Ed Weadon <ed@weadon.net>

--- wk3/ex5.py
+++ wk3/ex5.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+from netmiko import ConnectHandler
 import yaml
 
 # read in connection file
@@ -5,5 +7,8 @@ filename = input("Enter connection filename: ")
 
 with open(filenmae) as f:
     connections = yaml.load(f)
+
+net_conn = ConnectHandler(**connections['cisco3'])
+print(net_conn.find_prompt())
 
 
